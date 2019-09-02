f7f853d1d38f5f54e8663ebd5e888187088851dc
Brendon Bitencourt Braga <brendon.braga@gmail.com>
2019-09-02 20:54:31 -0300

    Set Model

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by Brendon Bitencourt Braga on 2019-09-02.
+//  Copyright © 2019 Brendon Bitencourt Braga. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by Brendon Bitencourt Braga on 2019-09-02.
+//  Copyright © 2019 Brendon Bitencourt Braga. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    let title: String
+    let instructions: String
+    let imageName: String
+}
